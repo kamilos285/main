acf9fb9835a93904d27d88b9b68bc679445bf68f
kamilos285 <kamilos285@gmail.com>
2024-03-24 10:23:09 +0100

    zadanie z km

--- kurs1.py
+++ kurs1.py
@@ -1,5 +1,18 @@
-print("hello")
 
-print("5")
 
-print("witam")
+OdlegloscKM = float(input("wprowadz odleglosc w km: "))
+
+
+OdlegloscCM = OdlegloscKM * 100000
+
+OdlegloscM = OdlegloscKM * 1000
+
+OdlegloscMM = OdlegloscKM * 1000000
+
+OdlegloscINCH = OdlegloscKM * 39370.0787
+
+OdlegloscFEET = OdlegloscKM * 3280.8399
+
+OdlegloscMile = OdlegloscKM * 0.621371192
+
+print(OdlegloscKM,"to", OdlegloscCM, "Cm", OdlegloscM,"M", OdlegloscMM, "MM", OdlegloscINCH,"cali", OdlegloscFEET,"stop", OdlegloscMile,"mili")
